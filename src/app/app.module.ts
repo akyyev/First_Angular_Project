440619bc6d2ca63ccfbc358ff40f470396846b37
Bagtyyar Akyyev <ba@Bagtyyars-MacBook-Pro.local>
2021-02-08 10:31:12 -0500

    formGroup, formArray, formControl

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { InputFormatDirective } from './input-format.directive';
 import { ZippyComponent } from './zippy/zippy.component';
 import { ContactFormComponent } from './contact-form/contact-form.component';
 import { SignupFormComponent } from './signup-form/signup-form.component';
+import { NewCourseFormComponent } from './new-course-form/new-course-form.component';
 
 @NgModule({
   declarations: [
@@ -32,7 +33,8 @@ import { SignupFormComponent } from './signup-form/signup-form.component';
     InputFormatDirective,
     ZippyComponent,
     ContactFormComponent,
-    SignupFormComponent
+    SignupFormComponent,
+    NewCourseFormComponent
   ],
   imports: [
     BrowserModule, // Contians CommonModule for Pipe stuff
---
+++ src/app/new-course-form/new-course-form.component.ts
@@ -0,0 +1,49 @@
+import { FormArray, FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'new-course-form',
+  templateUrl: './new-course-form.component.html',
+  styleUrls: ['./new-course-form.component.css']
+})
+export class NewCourseFormComponent {
+
+  form = new FormGroup({
+    topics : new FormArray([]),
+    name: new FormControl('', Validators.required),
+    contact: new FormGroup({
+      email: new FormControl(),
+      phone: new FormControl()
+    })
+  })
+
+  get topics() {
+    return this.form.get('topics') as FormArray;
+  }
+
+  addTopic(topic: HTMLInputElement){
+    this.topics.push(new FormControl(topic.value))
+    topic.value=''
+  }
+
+
+  removeTopic(topic: FormControl) {
+    const index = this.topics.controls.indexOf(topic);
+    this.topics.removeAt(index);
+  }
+
+
+
+  // This is an alternative to above decleration of form formGroup
+  formAlternative
+  constructor(fb: FormBuilder) {
+    this.formAlternative = fb.group({
+      name: ['', Validators.required],
+      contact: fb.group({
+        email: [],
+        phone: []
+      }),
+      topics: fb.array([])
+    })
+  }
+}
